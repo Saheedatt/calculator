906b693e92278a7a7d7c2d5a1e71a925516e8422
saheedatt <afolabisaheedat1@gmail.com>
2023-08-25 20:47:30 +0100

    Created readme & updated styling

---
+++ README.md
@@ -0,0 +1,36 @@
+# CALCULATOR PROJECT
+
+This is a simple calculator project created using HTML, CSS, and JavaScript. The calculator allows users to perform basic arithmetic calculations, use trigonometric functions, and a few other operators in maths.
+
+## Features
+
+- Basic arithmetic operations: addition, subtraction, multiplication, division
+- Exponentiation (^) and factorial (!) operations
+- Trigonometric functions: sin, cos, tan, and their inverses (sin⁻¹, cos⁻¹, tan⁻¹)
+- Logarithmic functions: log base 10 and natural logarithm (ln)
+- Square root (√) operation
+- Constants: π (pi)
+- Backspace (del) to delete the last entered character
+- Clear (Clear) to clear the input
+- Responsive design for different screen sizes
+
+## Technologies Used
+
+- HTML: Markup structure of the calculator
+- CSS: Styling and layout of the calculator and buttons
+- JavaScript: Logic for calculator operations and interactivity
+
+
+
+## Demo
+
+If you want to see a live demo of the calculator, you can visit [Demo Link](https://saheedatt.github.io/calculator/).
+
+## Screenshots
+
+![Calculator Screenshot](calculator.png)
+
+## Known Issues
+
+- I was unable to get the inverse of the trigonometry functions to work.
+
--- script.js
+++ script.js
@@ -12,66 +12,67 @@ buttons.forEach(button => {
             try {
                 let expression = display.value.replace(/\^/g, "**");
                 expression = expression.replace(/❨/g, '(').replace(/❩/g, ')');
-                let result = eval(expression);
-
-                if (trigFunctionToUse === "sin") {
-                    if (inverseMode) {
-                        result = Math.asin(result); // Inverse sine in radians
-                    } else {
-                        result = Math.sin(result * (Math.PI / 180)); // Sine in degrees
-                    }
-                } else if (trigFunctionToUse === "cos") {
-                    if (inverseMode) {
-                        result = Math.acos(result); // Inverse cosine in radians
-                    } else {
-                        result = Math.cos(result * (Math.PI / 180)); // Cosine in degrees
-                    }
-                } else if (trigFunctionToUse === "tan") {
-                    if (inverseMode) {
-                        result = Math.atan(result); // Inverse tangent in radians
-                    } else {
-                        result = Math.tan(result * (Math.PI / 180)); // Tangent in degrees
-                    }
-                    
-                }
                 
-
+                let result;
+                
+                if (trigFunctionToUse) {
+                    if (inverseMode) {
+                        result = applyInverseTrigFunction(expression);
+                    } else {
+                        result = applyTrigFunction(expression);
+                    }
+                } else {
+                    result = eval(expression);
+                }
+        
                 display.value = numberFormatter.format(result);
-
             } catch (error) {
                 display.value = "Error";
-            }
-        } else if (buttonText === "del") {
+            }        
+        }
+        else if (buttonText === "del") {
             display.value = display.value.slice(0, -1);
-        } else if (buttonText === "Clear") {
+        }
+        else if (buttonText === "Clear") {
             display.value = "";
             trigFunctionToUse = ""; 
-        } else if (buttonText === "sin" || buttonText === "cos" || buttonText === "tan") {
+            inverseMode = false;
+        }
+        else if (buttonText === "sin" || buttonText === "cos" || buttonText === "tan") {
             if (inverseMode) {
                 trigFunctionToUse = "a" + buttonText; 
-                display.value += buttonText + "⁻¹"; // Add inverse notation
-            } else {
-                trigFunctionToUse = buttonText; // Store the trigonometric function
-                display.value += buttonText;
-                console.log(trigFunctionToUse)
+                display.value += buttonText + "⁻¹"; 
             }
-        } else if (buttonText === "log") {
+            else {
+                trigFunctionToUse = buttonText; 
+                display.value += buttonText;
+            }
+        }
+        else if (buttonText === "log") {
             display.value = Math.log10(parseFloat(display.value));
-        } else if (buttonText === "ln") {
+        }
+        else if (buttonText === "ln") {
             display.value = Math.log(parseFloat(display.value));
-        } else if (buttonText === "π") {
+        }
+        else if (buttonText === "π") {
             display.value += Math.PI;
-        } else if (buttonText === "x") {
+        }
+        else if (buttonText === "x") {
             display.value += "*";
-        } else if (buttonText === "^") {
+        }
+        else if (buttonText === "^") {
             display.value += "**";
-        } else if (buttonText === "!") {
+        }
+        else if (buttonText === "!") {
             display.value = factorial(parseFloat(display.value));
-        } else if (buttonText === "√") {
+        }
+        else if (buttonText === "√") {
             display.value = Math.sqrt(parseFloat(display.value));
-        } else if (buttonText === "inv") {
-            inverseMode = !inverseMode; // Toggle inverse mode
-        } else {
+        }
+        else if (buttonText === "inv") {
+            inverseMode = !inverseMode; 
+        }
+        else {
             display.value += buttonText;
         }
     });
@@ -85,9 +86,31 @@ function factorial(num) {
     }
 }
 
-function reciprocal(number) {
-    if (number === 0) {
-        return "Undefined";
+function applyTrigFunction(expression) {
+    let radians = parseFloat(expression) * (Math.PI / 180);
+    switch (trigFunctionToUse) {
+        case "sin":
+            return Math.sin(radians);
+        case "cos":
+            return Math.cos(radians);
+        case "tan":
+            return Math.tan(radians);
+        default:
+            return eval(expression);
     }
-    return 1 / number;
 }
+function applyInverseTrigFunction(expression) {
+    let value = parseFloat(expression) * (180 / Math.PI);
+    switch (trigFunctionToUse) {
+        case "asin":
+            return Math.asin(value);
+        case "acos":
+            return Math.acos(value);
+        case "atan":
+            return Math.atan(value); 
+        default:
+            return eval(expression);
+    }
+}
+
+
